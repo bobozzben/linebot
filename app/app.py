acacaf2c5e95b62f26bbde0f562e0f1db1531553
ben <ben@hi-square.com.tw>
2021-05-19 10:07:44 +0800

    Modify

--- app/app.py
+++ app/app.py
@@ -33,10 +33,27 @@ for c in cur.fetchall():
 conn.close()
 
 
+#這是註冊 line 給的 
+channel_token = "LnaF+kFrRaEfCpGTeHmjCaZuHEI77T7VdHYJfrYxr3UFN/KL5Cb+TAhQJWYld2U2Cjx9CLkuwSclMcxupjsYBnQmz7NCuq0dey8IkjB2ZQXEGfs1oyReCIlJ44/Nats/972/aoApYbjDFE1GjgAa1AdB04t89/1O/w1cDnyilFU="
+channel_secret = "3604eceb8524e24dd7eede2dcfbd95d1"
+
+if channel_secret is None:
+   print('Set LINE_CHANNEL_SECRET as environment variable.')
+   sys.exit(1)
+if channel_token is None:
+   print('Set LINE_CHANNEL_ACCESS_TOKEN as environment variable.')
+   sys.exit(1)
+
+line_bot_api = LineBotApi(channel_token)
+handler = WebhookHandler(channel_secret)
+
+# https://a2ee41a6d68a.ngrok.io/bot_wsgi/linebot   && 這是給line Webhook settings 的 RUL, USE Webhook 要打勾
+
+@xapp.route("/linebot", methods=['POST'])
 def callback():
     signature = request.headers['X-Line-Signature']
     body = request.get_data(as_text=True)
-    app.logger.info("Request body: " + body)    
+    handler.handle(body, signature)
     try:
         print("body: "+body, signature)
         handler.handle(body, signature)        
@@ -45,49 +62,22 @@ def callback():
     return 'OK'
 
 # # 學你說話
-# @handler.add(MessageEvent, message=TextMessage)
-# def pretty_echo(event):
-    # print( "user_id :" + event.source.user_id )
-    # #因為LINE有些預設資料,我們在此排除
-    # if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef": 
-       # input_text = event.message.text
-       
-       # print("AAA" + input_text)
-       # if input_text == '查詢匯率':
-            # print("BBB" + input_text)
-            # resp = requests.get('https://tw.rter.info/capi.php')
-            # currency_data = resp.json()
-            # usd_to_twd = currency_data['USDTWD']['Exrate']
-            # print(usd_to_twd)
-       # line_bot_api.reply_message(
-          # event.reply_token,
-          # TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}')   )
+@handler.add(MessageEvent, message=TextMessage)
+def pretty_echo(event):
+    print( "user_id :" + event.source.user_id )
+    #因為LINE有些預設資料,我們在此排除
+    if event.source.user_id != "Udeadbeefdeadbeefdeadbeefdeadbeef": 
+       input_text = event.message.text       
+       print("AAA" + input_text)
+       if input_text == '查詢匯率':
+          print("BBB" + input_text)
+          resp = requests.get('https://tw.rter.info/capi.php')
+          currency_data = resp.json()
+          usd_to_twd = currency_data['USDTWD']['Exrate']
+          print(usd_to_twd)
+          line_bot_api.reply_message( event.reply_token
+             , TextSendMessage(text=f'美元 USD 對台幣 TWD：1:{usd_to_twd}')   )
 
-     
-        # # Phoebe 愛唱歌
-    # #    pretty_note = '♫♪♬'
-    # #    pretty_text = ''
-        
-    # #    for i in event.message.text:        
-    # #        pretty_text += i
-    # #        pretty_text += random.choice(pretty_note)      
-
-    
-     # #   line_bot_api.reply_message(
-     # #       event.reply_token,
-     # #       TextSendMessage(text=pretty_text)
-     # #   )
-        
-     # #   message = StickerSendMessage(
-     # #        package_id='1',
-     # #        sticker_id='1'
-     # #   )
-     # #   line_bot_api.reply_message(event.reply_token, message)     
-
-
-def index():
-    return 'Flask app is running'
-	
 if __name__ == "__main__":
    app.run(debug=True)
 
